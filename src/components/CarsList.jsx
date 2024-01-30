9d52699ff455863a7418451a4cf2a31b00ee247d
Emerson <emerson.roncancio@uniminuto.edu.co>
2024-01-29 23:40:45 -0500

    Nuevas trajetas del inventario

--- src/components/CarsList.jsx
+++ src/components/CarsList.jsx
@@ -1,10 +1,10 @@
 import { RiEmotionSadLine } from 'react-icons/ri'
 import { Link } from 'react-router-dom'
-import { Button, Card, Pagination } from 'keep-react'
-import { AiOutlineCar, AiOutlineTags } from 'react-icons/ai'
+import { Pagination } from 'keep-react'
 import { GrConfigure } from 'react-icons/gr'
-import { SlSpeedometer } from 'react-icons/sl'
 import { useState } from 'react'
+import { FaCarAlt } from "react-icons/fa";
+import { MdOutlineAttachMoney } from "react-icons/md";
 
 export default function CarsI ({ result }) {
   const [currentPage, setCurrentPage] = useState(1)
@@ -30,52 +30,51 @@ export default function CarsI ({ result }) {
         prevNextShape='circle'
       />
 
-      <div className='h-full mb-14 mt-[1.5rem] grid-col-res justify-center justify-items-center xl:mx-32 md:mx-5 gap-6 p-5'>
+      <div className='h-full mb-14 mt-[1.5rem] grid-col-res justify-center justify-items-center xl:mx-32 md:mx-5 gap-4 p-5'>
         {result.map((car) => (
-          <Card
-            key={car._id}
-            className='!max-w-xs overflow-hidden rounded-md md:!max-w-[480px]'
-            imgSrc={car.preview || car.images[0]}
-            imgSize='md'
-            horizontal
-          >
-            <Card.Container className='space-y-5 p-3'>
-              <Card.Title className='flex items-center gap-2 text-body-5 font-medium text-metal-500 md:!text-body-4'>
-                <SlSpeedometer className='mr-2' size={24} />
-                <span className='uppercase'>{car.line}</span>
-              </Card.Title>
-              <Card.Container className='grid grid-cols-2 gap-4'>
-                <Card.Title className='flex items-center gap-2 !text-body-5 font-medium text-metal-500'>
-                  <AiOutlineTags className='mr-2' size={24} />
-
-                  <span>{car.brand}</span>
-                </Card.Title>
-                <Card.Title className='flex items-center gap-2 !text-body-5 font-medium text-metal-500'>
-                  <GrConfigure className='mr-2' size={24} />
-                  <span>{car.model}</span>
-                </Card.Title>
-              </Card.Container>
-              <Card.Container className='grid grid-cols-2 gap-4'>
-                <Card.Title className='flex items-center gap-2 !text-body-5 font-medium text-metal-500'>
-                  {/* <GrConfigure className='mr-2' size={24} /> */}
-                  <SlSpeedometer className='mr-2' size={24} />
-                  <span>{car.kilometers}</span>
-                </Card.Title>
-                <Card.Title className='flex items-center gap-2 !text-body-5 font-medium text-metal-500'>
-                  <AiOutlineCar className='mr-2' size={24} />
-                  <span className='capitalize'>{car.type}</span>
-                </Card.Title>
-              </Card.Container>
-              <Card.Container className='my-3 grid grid-cols-1 gap-4'>
-                <Card.Title className='text-body-3 font-medium text-metal-500'>COP {Math.round(car.price).toLocaleString()}</Card.Title>
-                <Link className='w-auto' to={`/cars/${car._id}`}>
-                  <Button type='primary' className='w-full bg-blue-600' size='sm'>
-                    Ver más
-                  </Button>
-                </Link>
-              </Card.Container>
-            </Card.Container>
-          </Card>
+          <div 
+            key={car.id}
+            className='bg-blue-50 w-[87%] h-[390px] flex flex-col justify-start items-center relative rounded-3xl overflow-hidden mb-3 hover:bg-blue-200 duration-1000'>
+              <div className='w-full h-[45%] z-[9] rounded-md overflow-hidden '>
+                <img className='w-full h-full rounded-md object-cover' src={car.preview || car.images[0]} alt="" />
+              </div>
+              
+              <div className='absolute w-full h-full bg-transparent flex justify-center items-end z-[10]'>
+                <div className='w-[80%] h-[65%] flex justify-center items-center z-10'>
+                  <div className='w-full h-[80%] bg-white flex flex-col justify-around items-center rounded-xl px-2 shadow-[0_5px_17px_#A8BDF5] gap-1 py-1'>
+                    <div className='flex flex-col content-center items-center'>
+                      <h1 className='text-black font-sans text-lg text-wrap text-center'>{car.brand} {car.line}</h1>
+                    </div>
+                    <div className='flex gap-x-5'>
+                      <div className='flex flex-col content-center items-center'>
+                      <p className='text-[10px] text-blue-500 font-sans font-black'><strong>AÑO</strong></p>
+                        <div className='flex justify-center text-wrap content-center items-center'>
+                          <GrConfigure className='mr-2 text-blue-950' size={20} />
+                          <p className='text-black font-sans text-sm'>{car.model}</p>
+                        </div>
+                      </div>
+                      <div className='flex flex-col content-center items-center'>
+                      <p className='text-[10px] text-blue-500 font-sans font-black'><strong>KML</strong></p>
+                        <div className='flex justify-center text-wrap content-center items-center'>
+                          <FaCarAlt className='mr-2 text-blue-950' size={20} />
+                          <p className='text-black font-sans text-sm'>{car.kilometers}</p>
+                        </div>
+                      </div>
+                    </div>
+                    <div className='flex flex-col content-center items-center'>
+                      <p className='text-[10px] text-blue-500 font-sans font-black'><strong>PRECIO</strong></p>
+                        <div className='flex justify-center content-center items-center'>
+                          <MdOutlineAttachMoney className=' text-blue-950' size={20}/>
+                          <h1 className='text-black font-sans text-lg'>{Math.round(car.price).toLocaleString()}</h1>
+                        </div>
+                    </div>
+                    <Link className='px-4 py-1 border-2 border-blue-950 rounded-md bg-blue-950 text-white mb-1' to={`/cars/${car._id}`}>
+                      <p className='font-sans'>Ver mas</p>
+                    </Link>
+                  </div>
+                </div>
+              </div>
+          </div>
         )
         )}
       </div>
--- src/views/Cars.jsx
+++ src/views/Cars.jsx
@@ -39,11 +39,11 @@ export default function page () {
       <section className='image-cars flex flex-col justify-center text-center w-full rounded-b-lg lg:h-[60vh] h-[55vh] bg-center bg-cover bg-fixed overflow-hidden'>
         <h1 className='text-white text-4xl font-bold'>NUESTROS  AUTOS</h1>
       </section>
-      <section className='w-full bg-transparent max-[1920px]:top-[64px] min-[2560px]:top-[91px] flex justify-center sticky z-10'>
+      <section className='w-full bg-transparent max-[1920px]:top-[64px] min-[2560px]:top-[91px] flex justify-center sticky z-20'>
         <div className='flex z-20 w-[97%] lg:w-3/4 justify-center bg-blue-400 p-4 shadow-xl rounded-b-lg'>
           <form className='flex rounded-md overflow-hidden bg-transparent max-md:w-full md:w-[50%]' onSubmit={onSubmit}>
-            <Input className='max-md:text-xs h-full bg-white text-black dark:text-black w-[90%] border-solid rounded-none' placeholder='Buscar por marca, linea, año y color' value={bus} onChange={InputChange} type='text' />
-            <Button className='w-[10%] border-solid h-full rounded-none grid place-content-center bg-blue-500'><LuSearch size={15} /> </Button>
+            <Input className='max-md:text-xs h-full bg-white text-black dark:text-black w-[90%] border-solid rounded-none z-20' placeholder='Buscar por marca, linea, año y color' value={bus} onChange={InputChange} type='text' />
+            <Button className='w-[10%] border-solid h-full rounded-none grid place-content-center bg-blue-500 -z-20'><LuSearch size={15} /> </Button>
           </form>
         </div>
       </section>
