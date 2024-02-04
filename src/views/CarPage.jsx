4d75fb601e1036bef978ee27cf1b69acc1675669
BeiGod <saidsamuel2006@hotmail.com>
2024-02-03 20:52:50 -0500

    Actualizar estilo de fondo del formulario de búsqueda en Cars.jsx

--- src/views/CarPage.jsx
+++ src/views/CarPage.jsx
@@ -7,7 +7,7 @@ import { LuFuel } from 'react-icons/lu'
 import { GrConfigure } from 'react-icons/gr'
 import { FaMapLocationDot } from 'react-icons/fa6'
 import { BsCardText, BsWhatsapp } from 'react-icons/bs'
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import cars from '../mocks/cars.json'
 import { FullscreenCarrousel } from '../components/FullscreenCarrousel'
@@ -20,19 +20,19 @@ export default function CarPage () {
   const [selectedImage, setSelectedImage] = useState('')
   const loading = false
 
-  // const getRandomCars = () => {
-  //   const randomCars = []
-  //   const carsCopy = [...cars]
+  const getRandomCars = useMemo(() => {
+    const randomCars = []
+    const carsCopy = [...cars]
 
-  //   while (randomCars.length < 5) {
-  //     if (carsCopy.length === 0) break
-  //     const random = Math.floor(Math.random() * carsCopy.length)
-  //     randomCars.push(carsCopy[random])
-  //     carsCopy.splice(random, 1)
-  //   }
+    while (randomCars.length < 5) {
+      if (carsCopy.length === 0) break
+      const random = Math.floor(Math.random() * carsCopy.length)
+      randomCars.push(carsCopy[random])
+      carsCopy.splice(random, 1)
+    }
 
-  //   return randomCars
-  // }
+    return randomCars
+  }, [])
 
   if (loading) return <Spinner color='text-blue-600' className='h-[80dvh]' />
 
@@ -59,7 +59,7 @@ export default function CarPage () {
 
   return (
     <>
-      <section className='w-full h-auto grid grid-cols-1 lg:grid-cols-2 pt-10 max-md:p-0 max-md:pt-20 md:pt-24 min-[2560px]:p-36'>
+      <section className='w-full px-28 h-auto grid grid-cols-1 lg:grid-cols-2 pt-10 max-md:p-0 max-md:pt-20 md:pt-24 min-[2560px]:p-36'>
         <div className='w-full h-auto min-[420px]:h-[70vh] sm:h-[90vh] lg:h-[78vh] flex flex-col items-center justify-center pt-5'>
           <div className='w-[90%] md:w-[80%] h-full sm:w-[78%] sm:h-[88%] lg:h-[90%] min-[1920px]:w-[80%] min-[1920px]:h-[80%] min-[2560px]:w-[80%] min-[2560px]:h-[90%] mb-11 lg:mb-9'>
             <div className='w-full max-w-fit h-[80%] border rounded-md overflow-hidden mb-2 flex items-center relative'>
@@ -72,7 +72,6 @@ export default function CarPage () {
                   </div>
                 ))}
               </div>
-
             </div>
             <div className='flex h-[20%] select-none bg-neutral-200 border rounded-md overflow-x-auto p-1'>
               {imagenes.map((i, index) => (
@@ -84,53 +83,53 @@ export default function CarPage () {
           </div>
         </div>
 
-        <div className='w-full h-auto sm:h-full flex items-center justify-center'>
-          <div className='w-[90%] h-auto sm:w-[78%] sm:h-[88%] md:w-[84%] md:h-[100%] lg:w-[95%] lg:h-[95%] min-[1920px]:w-[90%] min-[1920px]:h-[90%] border-2 border-blue-200 rounded-md p-2'>
-            <div className='w-full h-[20%] flex flex-col justify-center items-center sm:items-start border-b-2 border-blue-200'>
+        <div className='w-full h-auto min-[420px]:h-[70vh] sm:h-[90vh] lg:h-[78vh] flex flex-col items-center justify-center pt-5'>
+          <div className='w-[90%] md:w-[80%] h-full sm:w-[78%] sm:h-[88%] lg:h-[90%] min-[1920px]:w-[80%] min-[1920px]:h-[80%] min-[2560px]:w-[80%] min-[2560px]:h-[90%] mb-11 lg:mb-9'>
+            <div className='w-full h-[20%] flex flex-col justify-center items-center sm:items-start border-b-2 border-blue-800'>
               <h1 className=' font-semibold text-2xl min-[1920px]:text-4xl min-[2560px]:text-5xl'>{car.brand} {car.line}</h1>
-              <h1 className=' font-semibold text-2xl min-[1920px]:text-4xl min-[2560px]:text-5xl'><span className='text-blue-400'>$</span> {Math.round(car.price).toLocaleString()}</h1>
+              <h1 className=' font-semibold text-2xl min-[1920px]:text-4xl min-[2560px]:text-5xl'><span className='text-green-400'>$</span> {Math.round(car.price).toLocaleString()}</h1>
             </div>
             <div className='w-full h-[80%] flex flex-row items-center p-2'>
               <div className='w-[50%] h-full mr-2'>{/* Colmuna 1 */}
-                <div className='flex h-[20%] items-center border-b-2 border-blue-200 capitalize'>
+                <div className='flex h-[20%] items-center border-b-2 border-blue-800 capitalize'>
                   <AiOutlineTags className='mr-2' size={24} />
                   <p className='max-sm:text-sm min-[2560px]:text-3xl min-[1920px]:text-xl'> <span className='text-blue-400 max-sm:text-xs text-xs min-[1920px]:text-base min-[2560px]:text-lg capitalize'>Marca</span>  <br /> {car.brand}</p>
                 </div>
-                <div className='flex h-[20%] items-center border-b-2 border-blue-200 capitalize'>
+                <div className='flex h-[20%] items-center border-b-2 border-blue-800 capitalize'>
                   <IoColorFilterOutline className='mr-2' size={24} />
                   <p className='max-sm:text-sm min-[2560px]:text-3xl min-[1920px]:text-xl'> <span className='text-blue-400 max-sm:text-xs text-xs min-[1920px]:text-base min-[2560px]:text-lg capitalize'>Color</span>  <br /> {car.color}</p>
                 </div>
-                <div className='flex h-[20%] items-center border-b border-blue-200 capitalize'>
+                <div className='flex h-[20%] items-center border-b-2 border-blue-800 capitalize'>
                   <AiOutlineFolderOpen className='mr-2' size={24} />
                   <p className='max-sm:text-sm min-[2560px]:text-3xl min-[1920px]:text-xl'> <span className='text-blue-400 max-sm:text-xs text-xs min-[1920px]:text-base min-[2560px]:text-lg capitalize'>Tipo</span>  <br /> {car.type}</p>
                 </div>
-                <div className='flex h-[20%] items-center border-b-2 border-blue-200 capitalize'>
+                <div className='flex h-[20%] items-center border-b-2 border-blue-800 capitalize'>
                   <TbSettingsCheck className='mr-2' size={24} />
                   <p className='max-sm:text-sm min-[2560px]:text-3xl min-[1920px]:text-xl'> <span className='text-blue-400 max-sm:text-xs text-xs min-[1920px]:text-base min-[2560px]:text-lg capitalize'>Transmision</span>  <br /> {car.transmission}</p>
                 </div>
-                <div className='flex h-[20%] items-center border-b-2 border-blue-200 capitalize'>
+                <div className='flex h-[20%] items-center border-b-2 border-blue-800 capitalize'>
                   <LuFuel className='mr-2' size={24} />
                   <p className='max-sm:text-sm min-[2560px]:text-3xl min-[1920px]:text-xl'> <span className='text-blue-400 max-sm:text-xs text-xs min-[1920px]:text-base min-[2560px]:text-lg capitalize'>Combustible</span>  <br /> {car.fuel}</p>
                 </div>
               </div>
               <div className='w-[50%] h-full ml-2'>{/* Colmuna 2 */}
-                <div className='flex h-[20%] items-center border-b-2 border-blue-200 capitalize'>
+                <div className='flex h-[20%] items-center border-b-2 border-blue-800 capitalize'>
                   <AiOutlineCar className='mr-2' size={24} />
                   <p className='max-sm:text-sm min-[2560px]:text-3xl min-[1920px]:text-xl'> <span className='text-blue-400 max-sm:text-xs text-xs min-[1920px]:text-base min-[2560px]:text-lg capitalize'>Linea</span>  <br /> {car.line}</p>
                 </div>
-                <div className='flex h-[20%] items-center border-b-2 border-blue-200 capitalize'>
+                <div className='flex h-[20%] items-center border-b-2 border-blue-800 capitalize'>
                   <SlSpeedometer className='mr-2' size={24} />
                   <p className='max-sm:text-sm min-[2560px]:text-3xl min-[1920px]:text-xl'> <span className='text-blue-400 max-sm:text-xs text-xs min-[1920px]:text-base min-[2560px]:text-lg capitalize'>Km</span>  <br /> {car.kilometers}</p>
                 </div>
-                <div className='flex h-[20%] items-center border-b-2 border-blue-200 capitalize'>
+                <div className='flex h-[20%] items-center border-b-2 border-blue-800 capitalize'>
                   <GrConfigure className='mr-2' size={24} />
                   <p className='max-sm:text-sm min-[2560px]:text-3xl min-[1920px]:text-xl'> <span className='text-blue-400 max-sm:text-xs text-xs min-[1920px]:text-base min-[2560px]:text-lg capitalize'>Año</span>  <br /> {car.model}</p>
                 </div>
-                <div className='flex h-[20%] items-center border-b-2 border-blue-200 capitalize'>
+                <div className='flex h-[20%] items-center border-b-2 border-blue-800 capitalize'>
                   <AiOutlineUserSwitch className='mr-2' size={24} />
                   <p className='max-sm:text-sm min-[2560px]:text-3xl min-[1920px]:text-xl'> <span className='text-blue-400 max-sm:text-xs text-xs min-[1920px]:text-base min-[2560px]:text-lg capitalize'>Propietarios</span>  <br /> {car.owners}</p>
                 </div>
-                <div className='flex h-[20%] items-center border-b-2 border-blue-200 capitalize'>
+                <div className='flex h-[20%] items-center border-b-2 border-blue-800 capitalize'>
                   <BsCardText className='mr-2' size={24} />
                   <p className='max-sm:text-sm min-[2560px]:text-3xl min-[1920px]:text-xl'> <span className='text-blue-400 max-sm:text-xs text-xs min-[1920px]:text-base min-[2560px]:text-lg capitalize'>Placa</span>  <br /> {car.plate}</p>
                 </div>
@@ -139,21 +138,21 @@ export default function CarPage () {
           </div>
         </div>
       </section>
-      <section className='w-full flex flex-col sm:flex-row justify-center items-center mb-5 mt-3'>
-        <Link className='bg-green-400 hover:bg-green-500 transition-colors p-3 text-white mr-2 max-sm:mr-0 rounded-md flex items-center justify-center max-sm:my-1' to='https://wa.me/573123719021' target='_blank' rel='noreferrer'><BsWhatsapp className='mx-1' size={25} /> <p>Contactar</p></Link>
-        <Link className='bg-blue-400 hover:bg-blue-500 transition-colors p-3 text-white rounded-md flex items-center justify-center max-sm:my-1' to='/about#maps'><FaMapLocationDot className='mx-1' size={25} /> <p>Ubicacion</p></Link>
+      <section className='w-full flex flex-col sm:flex-row justify-center items-center mb-5'>
+        <Link className='bg-green-400 hover:bg-green-500 transition-colors p-4 text-white mr-2 max-sm:mr-0 rounded-md flex items-center justify-center max-sm:my-1' to='https://wa.me/573123719021' target='_blank' rel='noreferrer'><BsWhatsapp className='mx-1' size={25} /> <p>Contactar</p></Link>
+        <Link className='bg-blue-400 hover:bg-blue-500 transition-colors p-4 text-white rounded-md flex items-center justify-center max-sm:my-1' to='/about#maps'><FaMapLocationDot className='mx-1' size={25} /> <p>Ubicacion</p></Link>
       </section>
 
       <FullscreenCarrousel open={open} handleClose={handleClose} selectedImage={selectedImage} data={imagenes} />
 
-      {/* <section className='flex gap-2'>
-        {getRandomCars().map(car => (
+      <section className='flex gap-4 justify-center my-4'>
+        {getRandomCars.map(car => (
           <div key={car._id}>
             <h1>{car.brand} {car.line}</h1>
-            <img src={car.preview || car.images[0]} alt='' width={200} />
+            <img src={car.preview || car.images[0]} alt='imgcar' width={200} height={200} className='max-h-[200px] max-w-[200px] object-contain' />
           </div>
         ))}
-      </section> */}
+      </section>
     </>
   )
 }
