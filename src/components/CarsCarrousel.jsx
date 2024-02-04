3906035e32cf78bb5912f4cef3b38c2c0632b0fa
Emerson Roncancio <roncancioavila@gmail.com>
2024-02-04 03:35:57 -0500

    actualizacion en seccion de ultimos autos

--- src/components/CarsCarrousel.jsx
+++ src/components/CarsCarrousel.jsx
@@ -1,48 +1,22 @@
-import { Carousel } from 'keep-react'
-import cars from '../mocks/cars.json'
+// import cars from '../mocks/cars.json'
 // import bgvideo from '../assets/carrouselbgvideo.webm'
 import Button from './Button'
-import { Link } from 'react-router-dom'
-import { IoOpenSharp } from 'react-icons/io5'
+import { TargetCar } from './TargetCar'
+import cars from '../mocks/cars.json'
 
 export default function CarsCarrousel () {
+  const carsFilter = cars.slice(0, 6)
+
   return (
-    <section className='w-full flex justify-center'>
-      <article className='imgFixed overflow-hidden w-[93%] flex justify-items-center content-center justify-center items-center relative h-[80vh] flex-col sm:flex-row bg-blue-950 rounded-[60px]'>
-        <div className='flex flex-col text-center space-y-4 w-[50%] items-center'>
-          <h2 className='text-4xl font-bold text-white' id='latest-vehicles'>
-            Nuestros últimos autos
-          </h2>
-          <p className='text-white max-w-[80%] text-base'>
-            Explora nuestra última colección de autos, nuevos y usados. Encuentra el equilibrio perfecto
-            entre innovación y confiabilidad. En nuestra concesionaria, descubre el automóvil ideal para
-            ti. ¡Vive la emoción de conducir con nosotros!
-          </p>
-          {/* <p className='text-base text-white'>Explora nuestro catálogo de vehículos tanto nuevos como usados.</p> */}
-          <Button className='bg-white text-blue-950 self-center hover:bg-slate-200'>Explorar</Button>
-        </div>
-        <div className='relative w-[50%] h-full bg-blue-950 flex justify-center items-center rounded-full p-7'>
-          <div className='absolute w-[78%] h-[78%] bg-slate-100 rounded-[40px]' />
-          <div className='flex justify-center h-[75%] w-[75%] items-center'>
-            <Carousel className='z-50' slideInterval={4000} showControls indicators>
-              {cars.slice(0, 5).map((car, index) => (
-                <article key={index} className='relative text-white'>
-                  <img src={car.preview || car.images[0]} alt='car' className='object-cover w-full h-full' />
-                  <span className='absolute md:text-xl flex flex-col md:justify-end p-4 bg-black/30 w-full h-full'>
-                    <h4>{car.brand}</h4>
-                    <p>{car.line}</p>
-                    <p>{car.model}</p>
-                  </span>
-                  <Link to={`/cars/${car._id}`}>
-                    <Button className='absolute bottom-0 right-16 z-50 m-4 hidden md:block'>Ver más</Button>
-                    <Button className='absolute top-0 right-5 z-50 m-4 bg-black/60 block md:hidden'><IoOpenSharp /></Button>
-                  </Link>
-                </article>
-              ))}
-            </Carousel>
-          </div>
-        </div>
+    <section className='w-full flex flex-col justify-center'>
+      <article className='flex justify-center'>
+        <h2 className='text-4xl font-bold text-black' id='latest-vehicles'>
+          Nuestros últimos autos
+        </h2>
       </article>
+      <TargetCar carsJson={carsFilter} />
+      {/* <p className='text-base text-white'>Explora nuestro catálogo de vehículos tanto nuevos como usados.</p> */}
+      <Button className='bg-blue-950 text-white self-center hover:bg-blue-900'>Explorar</Button>
     </section>
 
   )
---
+++ src/components/TargetCar.jsx
@@ -0,0 +1,58 @@
+import { FaCarAlt } from 'react-icons/fa'
+import { MdOutlineAttachMoney } from 'react-icons/md'
+import { GrConfigure } from 'react-icons/gr'
+import { Link } from 'react-router-dom'
+
+export const TargetCar = ({ carsJson }) => {
+  return (
+    <div className='h-full mb-14 mt-[1.5rem] grid-col-res justify-center justify-items-center xl:mx-32 md:mx-5 gap-4 p-5'>
+      {carsJson.map((car) => (
+        <div
+          key={car.id}
+          className='bg-blue-50 w-[87%] h-[390px] flex flex-col justify-start items-center relative rounded-3xl overflow-hidden mb-3 hover:bg-blue-200 duration-1000 shadow-md shadow-blue-200'
+        >
+          <div className='w-full h-[45%] z-[9] rounded-md overflow-hidden '>
+            <img className='w-full h-full rounded-md object-cover' src={car.preview || car.images[0]} alt='' />
+          </div>
+
+          <div className='absolute w-full h-full bg-transparent flex justify-center items-end z-[10]'>
+            <div className='w-[80%] h-[65%] flex justify-center items-center z-10'>
+              <div className='w-full h-[80%] bg-white flex flex-col justify-around items-center rounded-xl px-2 shadow-[0_5px_17px_#A8BDF5] gap-1 py-1'>
+                <div className='flex flex-col content-center items-center'>
+                  <h1 className='text-black  text-lg text-wrap text-center'>{car.brand} {car.line}</h1>
+                </div>
+                <div className='flex gap-x-5'>
+                  <div className='flex flex-col content-center items-center'>
+                    <p className='text-[10px] text-blue-500  font-black'><strong>AÑO</strong></p>
+                    <div className='flex justify-center text-wrap content-center items-center'>
+                      <GrConfigure className='mr-2 text-blue-950' size={20} />
+                      <p className='text-black  text-sm'>{car.model}</p>
+                    </div>
+                  </div>
+                  <div className='flex flex-col content-center items-center'>
+                    <p className='text-[10px] text-blue-500  font-black'><strong>KML</strong></p>
+                    <div className='flex justify-center text-wrap content-center items-center'>
+                      <FaCarAlt className='mr-2 text-blue-950' size={20} />
+                      <p className='text-black  text-sm'>{car.kilometers}</p>
+                    </div>
+                  </div>
+                </div>
+                <div className='flex flex-col content-center items-center'>
+                  <p className='text-[10px] text-blue-500  font-black'><strong>PRECIO</strong></p>
+                  <div className='flex justify-center content-center items-center'>
+                    <MdOutlineAttachMoney className=' text-blue-950' size={20} />
+                    <h1 className='text-black  text-lg'>{Math.round(car.price).toLocaleString()}</h1>
+                  </div>
+                </div>
+                <Link className='px-4 py-1 border-2 border-blue-950 rounded-md bg-blue-950 text-white mb-1' to={`/cars/${car._id}`}>
+                  <p className=''>Ver mas</p>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      )
+      )}
+    </div>
+  )
+}
