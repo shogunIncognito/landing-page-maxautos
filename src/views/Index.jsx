95ebd9d1f23aacd4034cc892a9b8f7e834cd5a1f
BeiGod <saidsamuel2006@hotmail.com>
2024-02-07 07:13:43 -0500

    primer prueba en index con framermotion

--- src/views/Index.jsx
+++ src/views/Index.jsx
@@ -7,6 +7,8 @@ import Button from '../components/Button'
 import CarsCarrousel from '../components/CarsCarrousel'
 import herovideo from '../assets/maxautosvideo720.webm'
 import logo from '../assets/maxautosicon.png'
+import { useRef } from 'react'
+import { useInView } from 'framer-motion'
 
 export default function Index () {
   return (
@@ -51,45 +53,66 @@ export default function Index () {
   )
 }
 
-const CardHero = () => (
-  <>
-    <div className='flex md:h-[80vh] md:flex-row flex-col w-full md:items-center items-end'>
-      <div className='flex flex-col items-center justify-center md:w-1/2 bg-white p-14 text-[#212529] w-full h-[50vh] gap-5 md:gap-0'>
-        {/* <h1 className='md:text-5xl text-3xl font-bold'>Max Autos</h1> */}
-        <img className='w-[100px] h-[80px] xl:w-[130px] xl:h-[100px]' src={logo} alt='MaxAutos' />
-        <p className='mt-4 text-xs md:text-xl xl:text-xl 2xl:text-2xl bold opacity-75 text-wrap leading-relaxed max-w-2xl'>
-          Empresa 100% Llanera dedicada a la compra y venta de vehículos usados a nivel Nacional con experiencia en el
-          mercado de más de 10 años
-        </p>
+const CardHero = () => {
+  const ref = useRef(null)
+  const ref2 = useRef(null)
+  const isInView = useInView(ref, { once: true })
+  const isInView2 = useInView(ref2, { once: true })
+  return (
+    <>
+      <div ref={ref} className='flex md:h-[80vh] md:flex-row flex-col w-full md:items-center items-end'>
+        <div
+          className='flex flex-col items-center justify-center md:w-1/2 bg-white p-14 text-[#212529] w-full h-[50vh] gap-5 md:gap-0'
+          style={{
+            transform: isInView ? 'none' : 'translateX(-200px)',
+            opacity: isInView ? 1 : 0,
+            transition: 'all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.3s'
+          }}
+        >
+          {/* <h1 className='md:text-5xl text-3xl font-bold'>Max Autos</h1> */}
+          <img className='w-[100px] h-auto xl:w-[150px]' src={logo} alt='MaxAutos' />
+          <p className='mt-4 text-xs md:text-xl xl:text-xl 2xl:text-2xl bold opacity-75 text-wrap leading-relaxed max-w-2xl'>
+            Empresa 100% Llanera dedicada a la compra y venta de vehículos usados a nivel Nacional con experiencia en el
+            mercado de más de 10 años
+          </p>
+        </div>
+        <div className='w-[90%] h-[90%] md:w-1/2 flex justify-end items-end md:items-center content-center bg-blue-950 rounded-l-full py-4 md:py-0'>
+          <img
+            alt='Vista aérea de Max Autos'
+            className='h-[90%] w-[90%] rounded-l-full object-cover'
+            height='1080'
+            src={img}
+          />
+        </div>
       </div>
-      <div className='w-[90%] h-[90%] md:w-1/2 flex justify-end items-end md:items-center content-center bg-blue-950 rounded-l-full py-4 md:py-0'>
-        <img
-          alt='Vista aérea de Max Autos'
-          className='h-[90%] w-[90%] rounded-l-full object-cover'
-          height='1080'
-          src={img}
-        />
+      <div className='flex md:h-[80vh] md:flex-row flex-col-reverse w-full mb-40'>
+        <div className='w-[90%] md:w-1/2 flex justify-start items-center bg-blue-950 rounded-r-full py-4 md:py-0'>
+          <img
+            alt='Vista aérea de Max Autos'
+            className='h-[90%] w-[90%] object-cover rounded-r-full'
+            src={img2}
+          />
+        </div>
+        <div
+          ref={ref2}
+          style={{
+            transform: isInView2 ? 'none' : 'translateX(200px)',
+            opacity: isInView2 ? 1 : 0,
+            transition: 'all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.3s'
+          }}
+          className='flex flex-col items-center justify-center md:w-1/2 bg-white p-14 text-[#212529]'
+        >
+          <h1 className='md:text-5xl text-3xl font-bold'>Max Autos</h1>
+          <p className='mt-4 text-xs md:text-xl xl:text-xl 2xl:text-2xl  bold opacity-75 text-wrap leading-relaxed max-w-2xl'>
+            Nos dedicamos a generar la mejor experiencia en la compra y venta de vehículos usados.
+            Nuestra prioridad es establecer acuerdos basados en confianza, calidad, cumplimiento y
+            responsabilidad, asegurando la satisfacción total de nuestros clientes.
+          </p>
+          <Link to='/about'>
+            <Button className='mt-6 hover:bg-blue-900 text-[#f8f9fa] bg-blue-950'>Conoce más</Button>
+          </Link>
+        </div>
       </div>
-    </div>
-    <div className='flex md:h-[80vh] md:flex-row flex-col-reverse w-full mb-40'>
-      <div className='w-[90%] md:w-1/2 flex justify-start items-center bg-blue-950 rounded-r-full py-4 md:py-0'>
-        <img
-          alt='Vista aérea de Max Autos'
-          className='h-[90%] w-[90%] object-cover rounded-r-full'
-          src={img2}
-        />
-      </div>
-      <div className='flex flex-col items-center justify-center md:w-1/2 bg-white p-14 text-[#212529]'>
-        <h1 className='md:text-5xl text-3xl font-bold'>Max Autos</h1>
-        <p className='mt-4 text-xs md:text-xl xl:text-xl 2xl:text-2xl  bold opacity-75 text-wrap leading-relaxed max-w-2xl'>
-          Nos dedicamos a generar la mejor experiencia en la compra y venta de vehículos usados.
-          Nuestra prioridad es establecer acuerdos basados en confianza, calidad, cumplimiento y
-          responsabilidad, asegurando la satisfacción total de nuestros clientes.
-        </p>
-        <Link to='/about'>
-          <Button className='mt-6 hover:bg-blue-900 text-[#f8f9fa] bg-blue-950'>Conoce más</Button>
-        </Link>
-      </div>
-    </div>
-  </>
-)
+    </>
+  )
+}
