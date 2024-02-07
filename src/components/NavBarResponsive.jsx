8ece82b7cff67b92d637354fd4e57908618821f4
BeiGod <saidsamuel2006@hotmail.com>
2024-02-07 08:33:09 -0500

    redes en navresponsive y ajustes de espacio

--- src/components/NavBarResponsive.jsx
+++ src/components/NavBarResponsive.jsx
@@ -4,8 +4,9 @@ import iconresponsive from '../assets/maxautosicon.png'
 import { Link } from 'react-router-dom'
 import { useState } from 'react'
 import { AiFillHome } from 'react-icons/ai'
-import { FaCarAlt } from 'react-icons/fa'
+import { FaCarAlt, FaWhatsapp } from 'react-icons/fa'
 import maxautoswhite from '../assets/maxautoslogoblanco.png'
+import { InstagramIcon } from './Footer'
 
 export default function NavBarResponsive () {
   const [boton, setBoton] = useState(false)
@@ -30,33 +31,44 @@ export default function NavBarResponsive () {
           transform: `translateX(-${boton ? 0 : 200}%)`,
           zIndex: '200'
         }}
-        className='w-[75%] h-screen bg-slate-100 transition-transform duration-300 top-0 fixed rounded-b-md flex flex-col items-center justify-between z-[106] shadow-xl p-4 lg:hidden'
+        className='w-[75%] h-screen bg-slate-100 transition-transform duration-300 top-0 fixed rounded-b-md flex flex-col z-[106] shadow-xl p-4 lg:hidden'
       >
         <div className='w-full h-32 flex flex-col justify-center items-center top-0 p-2'>
           <img className='w-28 h-auto my-3' src={iconresponsive} alt='logo' />
           <hr className='w-full text-black' />
         </div>
-        <div className='w-full flex flex-col items-center'>
+        <div className='w-full flex-1 gap-2 mt-5 flex flex-col items-start'>
           <div onClick={() => setBoton(false)} className='flex w-[80%] items-center justify-center p-2 hover:bg-blue-300 rounded-md'>
-            <Link className='flex w-full h-full justify-center my-4 items-center' to='/'>
-              <AiFillHome size={30} />
+            <Link className='flex w-full h-full my-2 items-center' to='/'>
+              <AiFillHome size={24} />
               <p className='flex items-center ml-3'>Inicio</p>
             </Link>
           </div>
           <div onClick={() => setBoton(false)} className='flex w-[80%] items-center justify-center p-2 hover:bg-blue-300 rounded-md'>
-            <Link className='flex w-full h-full justify-center my-4 items-center' to='/cars'>
-              <FaCarAlt size={30} />
+            <Link className='flex w-full h-full my-2 items-center' to='/cars'>
+              <FaCarAlt size={24} />
               <p className='flex items-center ml-3'>Inventario</p>
             </Link>
           </div>
           <div onClick={() => setBoton(false)} className='flex w-[80%] items-center justify-center p-2 hover:bg-blue-300 rounded-md'>
-            <Link className='flex w-full h-full justify-center my-4 items-center' to='/about'>
-              <RiTeamFill size={30} />
+            <Link className='flex w-full h-full my-2 items-center' to='/about'>
+              <RiTeamFill size={24} />
               <p className='flex items-center ml-3'>Nosotros</p>
             </Link>
           </div>
         </div>
-        <div onClick={() => setBoton(false)} className='w-full h-20 flex justify-center items-center bottom-0 p-2'>
+
+        <div onClick={() => setBoton(false)} className='w-full h-20 flex-col flex justify-center items-center bottom-0 p-2'>
+          {/* redes sociales */}
+          <div className='flex gap-3 mb-5'>
+            <a href='https://wa.me/573123719021' target='_blank' rel='noreferrer'>
+              <FaWhatsapp size={24} />
+            </a>
+            <a href='https://www.instagram.com/maxautosusados' target='_blank' rel='noreferrer'>
+              <InstagramIcon size={24} />
+            </a>
+          </div>
+
           <button onClick={() => setBoton(!boton)}>
             <ImCancelCircle size={35} />
           </button>
